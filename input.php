6033a3caefd4935e48b8e6cdef49c2c4f91a1e2f
daniel <maxis1314@tangmatoMacBook-Pro.local>
2018-02-03 23:32:28 +0800

    LIB:11

--- input.php
+++ input.php
@@ -2,6 +2,6 @@
 
 $date = date("Y-m-d H:i:s");
 $fp = fopen("tokens","a");
-fwrite($fp,$date."\t".$_GET['token']."\n");
+fwrite($fp,$date."\t$_SERVER[REMOTE_ADDR]\t".$_GET['token']."\n");
 fclose($fp);
 echo 1;
